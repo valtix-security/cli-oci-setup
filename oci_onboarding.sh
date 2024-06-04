42ca5f2362843987b71681f697ce731500205ba0
anikdash <anikdash@cisco.com>
2024-06-04 08:39:16 +0530

    handle child compartment path in policy document

--- oci_onboarding.sh
+++ oci_onboarding.sh
@@ -23,12 +23,67 @@ while getopts "hp:w:" optname; do
     esac
 done
 
+# Function to check if a compartment is active
+is_compartment_active() {
+    local compartment_id=$1
 
+    # Retrieve the lifecycle-state of the compartment
+    local compartment_state=$(oci iam compartment get --compartment-id "$compartment_id" --query 'data."lifecycle-state"' --raw-output)
+
+    # Return true (0) if the state is ACTIVE; false (1) otherwise
+    [ "$compartment_state" == "ACTIVE" ]
+}
+
+# Function to wait until a compartment is active
+wait_until_compartment_active() {
+    local compartment_id=$1
+
+    echo "Checking if compartment with OCID $compartment_id is active..."
+
+    # Wait for the compartment to become active
+    until is_compartment_active "$compartment_id"; do
+        echo "Compartment is not active yet. Waiting..."
+        sleep 10 # Wait for 10 seconds before checking again
+    done
+
+    echo "Compartment with OCID $compartment_id is now active."
+}
+
+# function to build path for compartemnt to be used in policy : example -> parent_compartment:child-compartmnet:grand-child-compartment 
+build_compartment_path() {
+    local compartment_id=$1
+    local path=""
+
+    # Loop to build the path up to the root compartment
+    while [ "$compartment_id" != "$TENANT_ID" ]; do
+        # Get the name of the compartment
+        compartment_details=$(oci iam compartment get --compartment-id $compartment_id 2>/dev/null)
+        compartment_name=$(echo "$compartment_details" | jq -r '.data.name')
+        compartment_parent_id=$(echo "$compartment_details" | jq -r '.data."compartment-id"')
+
+        # Break the loop if we've reached the top of the compartment hierarchy
+        if [ "$compartment_parent_id" == "null" ] || [ -z "$compartment_parent_id" ]; then
+            break
+        fi
+
+        # Prepend the compartment name to the path
+        path="${compartment_name}${path:+:$path}"
+
+        # Move to the parent compartment for the next iteration
+        compartment_id=$compartment_parent_id
+    done
+
+    # Return the path without the root compartment
+    echo "$path"
+}
 
 
 declare -a compartment_names
 
-read -p "Do you want to use any existing Compartment (y)  ? [y/n] " -n 1
+TENANT_ID=$(oci iam compartment list --all --compartment-id-in-subtree true --access-level ACCESSIBLE --include-root --raw-output --query "data[?contains(\"id\",'tenancy')].id | [0]")
+USING_ROOT_COMPARTMENT=false
+
+read -p "Do you want to use any existing Compartments (y)  ? [y/n] " -n 1
 
 existing_comp=$REPLY
 if [[ "$REPLY" == "y" || "$REPLY" == "Y" ]]; then
@@ -54,9 +109,9 @@ if [[ "$REPLY" == "y" || "$REPLY" == "Y" ]]; then
         length=${#display_name_map[@]}
         for ((i = 0; i < length; i++)); do
             display_name="${display_name_map[$i]}"
-            App_id="${compartment_id_map[$i]}"
+            id="${compartment_id_map[$i]}"
             
-            echo "$i $display_name/ $app_id"
+            echo "$i $display_name/ $id"
         done
         length=$(($length-1))
         read -p "Enter number from 0 - $length: " compartment_selection
@@ -67,14 +122,20 @@ if [[ "$REPLY" == "y" || "$REPLY" == "Y" ]]; then
         echo "Using the Compartment  ${display_name_map[$compartment_selection]} / ${compartment_id_map[$compartment_selection]}"
         COMPARTMENT_NAME=${display_name_map[$compartment_selection]}
         COMPARTMENT_ID=${compartment_id_map[$compartment_selection]}
-        compartment_names+=($(echo $COMPARTMENT_NAME | tr -d "\'"))
+
+        if [[ "$COMPARTMENT_ID" != "$TENANT_ID" ]]; then
+            compartment_names+=($(build_compartment_path "$COMPARTMENT_ID"))
+
+        else
+            USING_ROOT_COMPARTMENT=true
+        fi
 
         # remove the selected compartmet from the map 
         unset display_name_map[$compartment_selection]
         unset compartment_id_map[$compartment_selection]
 
         # Ask the user if they want to add a compartment
-        read -p "Do you want to add a more OCI compartments? (Y/N): " answer
+        read -p "Do you want to add more existing OCI compartments? (Y/N): " answer
 
         if [[ "$answer" == "n" || "$answer" == "N" || "$answer" == "no" ]]; then
             # User finished adding compartments, break out of the loop
@@ -84,7 +145,7 @@ if [[ "$REPLY" == "y" || "$REPLY" == "Y" ]]; then
 fi
      
 
-read -p "Do you want to add new compartment ? [y/n] " -n 1
+read -p "Do you want to create a new compartment ?[y/n] " -n 1
 
 new_comp=$REPLY
 if [[ "$REPLY" == "y" || "$REPLY" == "Y" ]]; then
@@ -122,12 +183,16 @@ if [[ "$REPLY" == "y" || "$REPLY" == "Y" ]]; then
     echo "selection $compartment_selection" 
     echo "Using the Compartment  ${display_name_map[$compartment_selection]} / ${compartment_id_map[$compartment_selection]} as root"
     echo "creating new compartment " $PREFIX-compartment
-    echo oci iam compartment create --name $PREFIX-compartment --description \"Created by Cisco Multicloud defence \" --compartment-id ${compartment_id_map[$compartment_selection]}
     result=$(oci iam compartment create --name $PREFIX-compartment --description "Created by Cisco Multicloud defence"  --compartment-id ${compartment_id_map[$compartment_selection]} --query "data.{"name":name,"id":id}")
     echo $result
     COMPARTMENT_NAME=$(echo $result | jq -r '.name | @sh')
     COMPARTMENT_ID=$(echo $result | jq -r '.id')
-    compartment_names+=("$(echo $result | jq -r '.name | @sh' | tr -d "\'")")
+
+    echo " waiting for compartment to be Active "
+    sleep 5
+    wait_until_compartment_active $COMPARTMENT_ID
+    #compartment_names+=("$(echo $result | jq -r '.name | @sh' | tr -d "\'")")
+    compartment_names+=($(build_compartment_path $COMPARTMENT_ID))
     echo created Compartment $COMPARTMENT_ID $COMPARTMENT_NAME
 fi
 
@@ -138,8 +203,6 @@ GROUP_NAME=$(echo $result | jq -r '.name | @sh' | tr -d "\'")
 GROUP_ID=$(echo $result | jq -r '.id')
 echo created Group  $GROUP_ID $GROUP_NAME
 
-TENANT_ID=$(oci iam compartment list --all --compartment-id-in-subtree true --access-level ACCESSIBLE --include-root --raw-output --query "data[?contains(\"id\",'tenancy')].id | [0]")
-
 echo 
 echo "Creating Policy" $PREFIX-controller-policy
 
@@ -148,7 +211,21 @@ policy_file="/tmp/policy.txt"
 # Start the JSON policy file with an opening bracket
 echo "[" > "$policy_file"
 
-# Loop over each compartment OCID and create policy statements
+if [[ "$USING_ROOT_COMPARTMENT" == "true" ]]; then
+    root_policy_statement="\"Allow group $GROUP_NAME to inspect instance-images in tenancy\",
+\"Allow group $GROUP_NAME to read app-catalog-listing in tenancy\",
+\"Allow group $GROUP_NAME to use volume-family in tenancy\",
+\"Allow group $GROUP_NAME to use virtual-network-family in tenancy\",
+\"Allow group $GROUP_NAME to manage volume-attachments in tenancy\",
+\"Allow group $GROUP_NAME to manage instances in tenancy\",
+\"Allow group $GROUP_NAME to {INSTANCE_IMAGE_READ}in tenancy\",
+\"Allow group $GROUP_NAME to manage load-balancers in tenancy\",
+\"Allow group $GROUP_NAME to manage app-catalog-listing in tenancy\","
+    echo "$root_policy_statement" >> "$policy_file"
+fi
+
+
+# Loop over each compartment name and create policy statements
 for COMPARTMENT_NAME in "${compartment_names[@]}"; do
     # Generate the policy statement and append it to the policy file
     policy_statement="\"Allow group $GROUP_NAME to inspect instance-images in compartment $COMPARTMENT_NAME\",
@@ -242,7 +319,8 @@ fi
 
 
 cleanup_file="delete-oci-cisco-mcd-setup.sh"
-echo "Create uninstaller script in the current directory '$cleanup_file' run script with region param"
+echo "Created uninstaller script in the current directory '$cleanup_file': Note run script with tennants base region parameter to delete compartment created with this script "
+echo " example : ./'$cleanup_file' us-sanjose-1 "
 
 if [[ "$new_comp" == "n"  || "$new_comp" == "N" ]]; then
 
@@ -287,7 +365,9 @@ echo
 echo "----------------------------------------------------------------------------------------------------"
 echo "Information shown below is needed to onboard subscription to the Cisco Multicloud Defense Controller"
 echo "----------------------------------------------------------------------------------------------------"
-echo "Tenant/Directory: $TENANT_ID"
-echo "    user_id: $USER_ID"    
-echo " Private key : $PRIVATE_KEY"   
+echo "Tenant/Directory  : $TENANT_ID"
+echo
+echo " User_Id          : $USER_ID"    
+echo
+echo " Private key      : $PRIVATE_KEY"   
 echo "----------------------------------------------------------------------------------------------------"
